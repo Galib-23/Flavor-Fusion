6401b609fec3ae71d37a830981c3bc78680257a4
Galib-23 <aagalib2323@gmail.com>
2023-11-09 19:52:40 +0600

    Featured

--- src/Pages/HomeComponents/Featured.jsx
+++ src/Pages/HomeComponents/Featured.jsx
@@ -6,7 +6,7 @@ const Featured = () => {
                 <img src={delivery} className="max-w-xs md:max-w-md  rounded-lg shadow-2xl border border-cyan-400 p-8" />
                 <div className='w-1/2'>
                     <h1 className="text-5xl font-bold text-yellow-300">Get Delivery Anywhere & Anytime !</h1>
-                    <p className="py-6 text-cyan-200">We cover a wide range of delivery location and the foods are delivered within time always. We also care about the delivery process so that the food to deliver never get damaged. If damaged you get a free return policy.</p>
+                    <p className="py-6 text-cyan-200">We cover a wide range of delivery location and the foods are delivered within time always. We also care about the delivery process so that the food to deliver never get damaged. If damaged you get a free return policy. We are always dedicated to customers</p>
                     <button className="btn btn-secondary btn-outline">Order Now</button>
                 </div>
             </div>
